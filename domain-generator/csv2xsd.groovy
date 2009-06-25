ad726859fa5d734b8d5d15e63d3c8f5cf6f2dce8
kent <kent@machina.no>
2009-06-25 18:47:33 +0200

    working incomplete xsd

--- domain-generator/csv2xsd.groovy
+++ domain-generator/csv2xsd.groovy
@@ -11,7 +11,7 @@ def xsd = builder.'xsd:schema'( targetNamespace: "http://www.machina.no/noark", 
 	while(lines.hasNext()){
 		def line = lines.next()
 		if(line.startsWith("Metadata for ")){
-			'xsd:complexType'(name: line.substring(13, line.indexOf(DELIM))){
+			'xsd:complexType'(name: camelize( line.substring(13, line.indexOf(DELIM)) )){
 				def state = "package"
 				'xsd:sequence'() {
 					println "complextype started"
@@ -23,7 +23,15 @@ def xsd = builder.'xsd:schema'( targetNamespace: "http://www.machina.no/noark", 
 						line = lines.next()
 						if(line == " ¤¤¤¤¤") break;
 						vals = line.split("¤")
-						'xsd:element'(name: vals[1], type: translateType(vals[5]))
+						def type = vals[5]
+						if(type.contains("/") && !type.contains("Dato")){
+							type.split("/").collect { t ->
+								def printableType = translateType(t)
+								'xsd:element'(name: "${camelize(vals[1])}_${t.substring(0,t.indexOf('.')).replace(' ','')}", type: printableType)
+							}
+						} else {
+							'xsd:element'(name: camelize(vals[1]), type: translateType(vals[5]))
+						}
 
 					}
 				}
@@ -33,7 +41,7 @@ def xsd = builder.'xsd:schema'( targetNamespace: "http://www.machina.no/noark", 
 	}
 }
 println writer.toString()
-new File("noark_5.xml").write(writer.toString())
+new File("noark_5.xsd").write(writer.toString())
 def translateType(type){
 	switch(type){
 		case ~/Tekststreng.*/:
@@ -42,12 +50,26 @@ def translateType(type){
 		case "Dato":
 			return "xsd:dateTime"
 		case ~/.*systemID/:
-			return "noark:${type - '.systemID'}"
+			return "noark:${camelize(((type - '.systemID') - '. systemID')-'.systemid')}"
 	}
 	println "unknown type: ${type}"
 	return "Unknown Type"
 }
 
-def spilit(line){
-
+def capitalize(delegate){
+	if(delegate == "") return ""
+	def chars = delegate.getChars()
+	chars[0] = chars[0].toUpperCase() 
+	return new String(chars)
 }
+
+def camelize(name){
+	def retval = name.split(" ").collect { word -> 
+    capitalize(word.toLowerCase())
+  }.join("")
+	if(retval == "Forenkletregistrering") return "ForenkletRegistrering"
+	return retval
+}
+
+
+
