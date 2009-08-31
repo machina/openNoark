3259b9dd79b03109ba3da74e6b0fb4205d9f8b21
kent <kent@machina.no>
2009-08-31 16:36:53 +0200

    filoverføring

--- tb-fangst/chrome/content/opennoark.js
+++ tb-fangst/chrome/content/opennoark.js
@@ -12,20 +12,44 @@ function catchDoc()
 
   else
     {
+			var item = selectedAttachments[0];
+			saveAndReadAttachment(item.attachment);
+		}
+
+}
+
+function catchDoc2(bytes){
 			var req = new XMLHttpRequest();
 			login(req);
 			var folderTxt = getFolders(req);
 			//alert(folderTxt);
 
-	    var item = selectedAttachments[0]
-
-  	  var attachmentName = {value: item.attachment.name};
+	    
+  	  //var attachmentName = {value: item.attachment.name};
 			var params = {inn:{regtype: "dilldall", folderTxt: folderTxt}, out:null};
 			window.openDialog("chrome://opennoark/content/opennoarkdiag.xul", "", 
 			"chrome, dialog, modal, resizable=yes", params).focus();
-			alert(params.out);
-			registerRegistration(req, params.out.folderId);
-    }
+			//alert(bytes);
+			//alert(params.out);
+			var reg = registerRegistration(req, params.out.folderId);
+			/*
+			var children = reg.childNodes;
+			var doc = "";
+			for (i=0;i<children.length;i++){
+				alert(children[i].tagName);
+				var sub = children[i].childNodes
+				for (j=0;j<sub.length;j++){
+					alert(sub[j].tagName);
+				}
+
+			}*/
+			var regId = reg.getElementsByTagName('systemid')[0].textContent;
+			alert("regId: "+regId);
+			var docobj =  registerDocObject(req, regId );
+			docobj = docobj.getElementsByTagName('systemid')[0].textContent;
+			archiveDoc(req, docobj, bytes);
+			alert("ferdig");
+    
 }
 
 
@@ -40,13 +64,13 @@ function login(req){
 function getFolders(req){
 	req.open("GET", "http://localhost:8080/openNoark/basismappe/list", false);
   req.setRequestHeader("Content-Type", "text/json");
-	req.setRequestHeader('Accept','text/xml'); 
+	req.setRequestHeader('Accept','text/json'); 
   req.send(null);
 	return req.responseText;
 }
 
 function registerRegistration(req, folderId){
-	alert(folderId);
+	//alert(folderId);
 	var reg = '<?xml version=\"1.0\"?>\n<ForenkletRegistrering>'
 						+'	<registreringstype>dilldall</registreringstype>'
 						+'	<opprettetdato>02-02-2002</opprettetdato>'
@@ -56,9 +80,44 @@ function registerRegistration(req, folderId){
 						+'	<mappe_id>'+folderId+'</mappe_id>'
 						+'</ForenkletRegistrering>';
 	req.open("POST", "http://localhost:8080/openNoark/remote/forenkletRegistrering", false);
-	req.setRequestHeader("Content-Type", "text/xml"); 
+	req.setRequestHeader("Content-Type", "text/xml");
+	req.setRequestHeader('Accept','text/json');
 	req.send(reg);
 	//alert(req.responseText);
+	return req.responseXML;
+}
+
+function registerDocObject(req, regId){
+  //alert(folderId);
+  var reg = '<?xml version=\"1.0\"?>\n<dokumentobjekt>'
+						+ '<versjonsnummer>1</versjonsnummer>'
+						+ '<variantformat>?</variantformat>'
+						+ '<format>?</format>'
+						+ '<formatdetaljer>?</formatdetaljer>'
+						+ '<opprettetdato>02-02-2002</opprettetdato>'
+            + '<opprettetav>meg</opprettetav>'
+						+ '<referansedokumentfil>?</referansedokumentfil>'
+						+ '<referanseregistrering>'+regId+'</referanseregistrering>'
+						+ '<filstørrelse>-1</filstørrelse>'
+						+ '<sjekksumalgoritme>none</sjekksumalgoritme>'
+						+ '<sjekksumdokument>?</sjekksumdokument>'
+						+ '<variantformat>?</variantformat>'
+						+ '<versjonsnummer>?</versjonsnummer>'
+            +'</dokumentobjekt>';
+  req.open("POST", "http://localhost:8080/openNoark/remote/dokumentobjekt", false);
+  req.setRequestHeader("Content-Type", "text/xml");
+	//req.setRequestHeader('Accept','text/json');
+  req.send(reg);
+  //alert(req.responseText);
+  return req.responseXML;
+}
+
+function archiveDoc(req, docObjId, doc){
+	req.open("POST", "http://localhost:8080/openNoark/fileStoage/save/"+docObjId, false);
+	var b64 = btoa(doc)
+	//req.setRequestHeader("Content-Length", b64.length);  
+	req.send(b64);  
+	alert(req.responseText);		
 }
 
 // Called once when the dialog displays
@@ -84,12 +143,88 @@ function onOK() {
    // Return the changed arguments.
    // Notice if user clicks cancel, window.arguments[0].out remains null
    // because this function is never called
-		alert("folderlist:"+document.getElementById("folderlist"));
-		alert("selectedItem"+document.getElementById("folderlist").selectedIndex);
-		alert("selectedItem"+document.getElementById("folderlist").selectedItem);
+		//alert("folderlist:"+document.getElementById("folderlist"));
+		//alert("selectedItem"+document.getElementById("folderlist").selectedIndex);
+		//alert("selectedItem"+document.getElementById("folderlist").selectedItem);
    window.arguments[0].out = {name:document.getElementById("regtype").value,
 				folderId: document.getElementById("folderlist").selectedItem.value};
    return true;
 }
 
+var TMP_DIR = "/tmp/";
 
+//Stolen shamelessly readonly attachments and https://developer.mozilla.org/en/Code_snippets/File_I%2F%2FO
+function saveAndReadAttachment(anAttachment) {
+   // get a handle to the temporary folder
+   var target = Components.classes["@mozilla.org/file/local;1"].
+                 createInstance(Components.interfaces.nsILocalFile);
+   target.initWithPath(TMP_DIR + "foo");
+
+   // save the attachment to the folder
+	
+   var savedFile = messenger.saveAttachmentToFolder(anAttachment.contentType,
+                             anAttachment.url,
+                             getFileName(anAttachment.displayName),
+                             anAttachment.messageUri, target);
+  setTimeout(saveAndReadSavedFile, 5000, savedFile);
+	
+}
+
+function saveAndReadSavedFile(savedFile) {
+	var fileSize = savedFile.fileSize
+
+	var istream = Components.classes["@mozilla.org/network/file-input-stream;1"].
+                        createInstance(Components.interfaces.nsIFileInputStream);
+	istream.init(savedFile, -1, -1, false);
+
+	var bstream = Components.classes["@mozilla.org/binaryinputstream;1"].
+                        createInstance(Components.interfaces.nsIBinaryInputStream);
+	bstream.setInputStream(istream);
+	//bstream.available();
+	//busyWait(10000);
+	alert("bstream.available "+bstream.available());
+	//while(bstream.available() <= 0){  }
+	var size = 0;
+	var file_data = "";
+	alert("filesize: "+fileSize);
+	while(file_data.length < fileSize) { 
+    file_data += bstream.readBytes(bstream.available()); 
+  }
+	alert("data  length"+file_data.length)
+	catchDoc2(file_data);
+	//var bytes = bstream.readBytes(bstream.available());
+	//return bytes
+
+} // saveAttachment
+
+
+function getFileName(baseName) {
+
+   // baseName = encodeURIComponent(baseName);
+   var newName = baseName;
+
+   // create a local file handler to check for file existence
+   var target = Components.classes["@mozilla.org/file/local;1"].
+                 createInstance(Components.interfaces.nsILocalFile);
+   target.initWithPath(TMP_DIR + newName);
+
+   count = 0;
+   while (target.exists()) {
+      count++;
+
+      newName = count + "_" + baseName;
+      target.initWithPath(TMP_DIR + newName);
+   } // while the file name already exists
+
+   return newName;
+
+} // getFileName
+
+
+function busyWait(millis){
+	var date = new Date();
+	var curDate = null;
+
+	do { curDate = new Date(); }
+	while(curDate-date < millis);
+}
