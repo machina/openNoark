590a042c572e68892471c4bc3c2a30f14cb153cd
kent <kent@machina.no>
2010-03-01 16:59:08 +0100

    generert kode legges nå i det aktive prosjektes src mappe pluss masse opprydninger

--- emf2gorm/emf2gorm/src/no/machina/openNoark/Ecore2Gorm.groovy
+++ emf2gorm/emf2gorm/src/no/machina/openNoark/Ecore2Gorm.groovy
@@ -1,98 +1,98 @@
 package no.machina.openNoark
 
+import java.io.ByteArrayInputStream;
+import java.io.StringWriter;
 import java.io.File;
-
+import java.io.StringReader;
+import org.eclipse.core.resources.*
 
 
 class Ecore2Gorm {
-
-	def transform(def pack){
+	
+	def transform(def pack, def projectName){
 		String packageName = getPackageName(pack);
 		def classes = pack.eContents()
 		classes.each{ tklass ->
-		def klass = tklass
-		if(tklass.getClass().toString().contains("GenClass") ) {
-			klass = tklass.getEcoreClass()
-		}
-		println klass.name
-		println klass
-
-		println klass.getClass().name
-		println "packageName: ${packageName}"
-		  StringWriter writer = new StringWriter()
-		  def builder = new GormBuilder(writer)
-		  def parent = klass.eSuperTypes.size() > 0 ? klass.eSuperTypes[0].name : null
-	      def documentation = ""
-		  builder."${klass.name}"(parent: parent, packageName: packageName){
-		    //println klass.eReferences
-		    //println klass.eAttributes
-		    klass.eContents().each {
-		      println it.getClass().name
-		      switch(it.getClass().name){
-		        case "org.eclipse.emf.ecore.impl.EAnnotationImpl":
-		        	 println "key ${it.details.key[0]}"
-		          	 //if(it.details.key[0] == "documentation") {
-		        	 //	  println "setting doc to ${it.details.value[0]}"
-		        	 //	  documentation = it.details.value[0]
-		        	 // } else {
-		        	 	if(it.source == "http://opennoark.machina.no/searchable"){
-		        	 		println "searchable"
-		        	 		builder.annotation(annotation: it)
-		        	 	}else {
-		        		  builder.annotation(key: it.details.key[0], value: it.details.value[0], details: it.details )
-		        	 	}
-		        	 // }
-		        	   
-		          break
-		        case "org.eclipse.emf.ecore.impl.EGenericTypeImpl":
-		          //TODO: these will have to be handled at some point
-		          break
-		        default:
-		          def mul = "1"
-		          if(it.isRequired()){
-		            if(it.isMany()) {
-		              mul = "1-M"
-		            } else {
-		              mul = "1"
-		            }
-		          } else {
-		            if(it.isMany()) {
-		              mul = "0-M"
-		            } else {
-		              mul = "0-1"
-		            }
-		          }
-		          
-		          println "building for ${it.name}" 
-		          "${it.name}"(type: translateType(it.eType.name) ,multiplicity: mul, unique: it.isUnique())
-		          break
-		      }//end switch
-		    } //end klass.eContents
-
-		  } //end builder
-
-		  println "writing to file, package: ${packageName}"
-		  try{
-			  File f = new File("/home/kent/out/domain/${packageName.split('.').join('/')}")
-			  boolean b = f.mkdirs()
-			  println "mkdirs: ${b}"
-			  f = new File("/home/kent/out/domain/${packageName.split('.').join('/')}/${klass.name}.groovy")
-			  
-			  f.write writer.toString()
-			  println "write successfull"
-		  }catch(Exception e){
-			  e.printStackTrace()
-		  }
-		  
-		  
+			def klass = tklass
+			if(tklass.getClass().toString().contains("GenClass") ) {
+				klass = tklass.getEcoreClass()
+			}
+			
+			StringWriter writer = new StringWriter()
+			def builder = new GormBuilder(writer)
+			def parent = klass.eSuperTypes.size() > 0 ? klass.eSuperTypes[0].name : null
+			def documentation = ""
+			builder."${klass.name}"(parent: parent, packageName: packageName){
+				klass.eContents().each {
+					
+					switch(it.getClass().name){
+						case "org.eclipse.emf.ecore.impl.EAnnotationImpl":
+						
+						if(it.source == "http://opennoark.machina.no/searchable"){
+							
+							builder.annotation(annotation: it)
+						}else {
+							builder.annotation(key: it.details.key[0], value: it.details.value[0], details: it.details )
+						}
+						
+						
+						break
+						case "org.eclipse.emf.ecore.impl.EGenericTypeImpl":
+						//TODO: these will have to be handled at some point
+						break
+						default:
+						def mul = "1"
+						if(it.isRequired()){
+							if(it.isMany()) {
+								mul = "1-M"
+							} else {
+								mul = "1"
+							}
+						} else {
+							if(it.isMany()) {
+								mul = "0-M"
+							} else {
+								mul = "0-1"
+							}
+						}
+						"${it.name}"(type: translateType(it.eType.name) ,multiplicity: mul, unique: it.isUnique())
+						break
+					}//end switch
+				} //end klass.eContents
+				
+			} //end builder
+			
+			
+			
+			try{ 
+				IProject project = org.eclipse.core.resources.ResourcesPlugin.getWorkspace().getRoot().getProject(projectName)
+				IFolder src = project.getFolder("src")
+				
+				IFolder pkg = src
+				packageName.tokenize('.').each{
+					pkg = pkg.getFolder(it)
+					if(!pkg.exists()) pkg.create false, false, null
+				}
+				IFile f = pkg.getFile("${klass.name}.groovy")//new File("src/${packageName.split('.').join('/')}")
+				if(f.exists()) f.delete true, null
+				ByteArrayInputStream is = new ByteArrayInputStream(writer.toString().getBytes());
+				f.create(is, true, null)
+				//f.write writer.toString()
+				
+				
+			}catch(Exception e){
+				e.printStackTrace()
+			}
+			
+			
 		}//end classes
 	}
 	def translateType(type) {
-	    if(type == "DateTime") return "Date"
+		if(type == "DateTime") return "Date"
 		if(type == null || type == "null") return "String"
 		return type
 	}
-
+	
 	String getPackageName(def pack){
 		def name = ""
 		if(pack.getClass().name == "org.eclipse.emf.codegen.ecore.genmodel.impl.GenPackageImpl") pack = pack.getEcorePackage()
--- emf2gorm/emf2gorm/src/no/machina/openNoark/GormBuilder.groovy
+++ emf2gorm/emf2gorm/src/no/machina/openNoark/GormBuilder.groovy
@@ -4,131 +4,126 @@ import groovy.util.BuilderSupport
 import java.io.StringWriter
 
 class GormBuilder extends BuilderSupport {
-		
-		final int CLASS = 1
-		final int ATTRS = 2
-
-		int state = CLASS
-
-		IndentPrinter out;
-		StringWriter writer;
-		def constaints = ""
-		def hasMany = [:]	
-		def mappings = ""
-		def extras = ""
-		def afterLoad = ""
-		def transients = []
-		def toString = ""
-		public GormBuilder(Writer writer) {
-			this.writer = writer 
-			this.out =	new IndentPrinter(new PrintWriter(writer))
-			def printlnMethod = this.out.&println
-			this.out.metaClass.println = {String s ->
-			    printIndent()
-			    printlnMethod.call(s)
-			}
+	
+	final int CLASS = 1
+	final int ATTRS = 2
+	
+	int state = CLASS
+	
+	IndentPrinter out;
+	StringWriter writer;
+	def constaints = ""
+	def hasMany = [:]	
+	def mappings = ""
+	def extras = ""
+	def afterLoad = ""
+	def transients = []
+	def toString = ""
+	public GormBuilder(Writer writer) {
+		this.writer = writer 
+		this.out =	new IndentPrinter(new PrintWriter(writer))
+		def printlnMethod = this.out.&println
+		this.out.metaClass.println = {String s ->
+			printIndent()
+			printlnMethod.call(s)
 		}
-
-	protected void setParent(Object parent, Object child) {  
-  	if(parent)  
-    	parent[parent['name']] = child  
 	}
-
-		
-   
-   protected Object createNode(Object name) {  
-     createNode name, null, null  
-   }  
-   
-   protected Object createNode(Object name, Object value) {  
-     createNode name, null, value  
-   }  
-   
-   protected Object createNode(Object name, Map attrs) {  
-     createNode name, attrs, null  
-   }  
-   
-   protected Object createNode(Object name, Map attrs, Object value) {
-			  switch(state){
-					case CLASS:
-							if(attrs && attrs['packageName']) out.println "package ${attrs['packageName']}"
-							out.print "class ${name} "
-							
-							if(attrs && attrs['parent']) out.print "extends ${attrs['parent']}"
-							out.println "{"
-							out.incrementIndent()
-							state = ATTRS
-						break
-					case ATTRS:
-						//println "name: ${name}"
-						//println "attrs: ${attrs}"
-						//out.printIndent()
-						if(name == "annotation"){
-							if(attrs['annotation']){
-								switch(attrs['annotation'].source){
-									case "http://opennoark.machina.no/searchable":
-										println "building http://opennoark.machina.no/searchable"
-										def searchable = "static searchable ="
-										attrs['annotation'].details.keySet().each{ skey ->
-											if(skey == "except")
-												searchable += " [except: ['${attrs['annotation'].details.get(skey).split(',').join('\',\'')}']]"
-										}
-										if(searchable == "static searchable =") searchable += " true"
-										extras = "${extras}${searchable}\n"
-										break
-								}
-							} else {
-								if(attrs['key'] == "documentation"){
-									writer.buffer.insert(0,"/**\n${attrs['value']}\n*/\n")
-								}else if(attrs['key'] == "mapping"){
-									println "adding mapping: ${attrs['value']}"	
-									mappings = "${mappings}${attrs['value']}\n"
-								}else if(attrs['key'] == "attribute"){
-									extras = "${extras}${attrs['value']}\n"
-								}else if(attrs['key'] == "transient"){
-									extras = "${extras}${attrs['value']}\n"
-									transients << "\"${attrs['value'].tokenize()[1]}\""
-								}else if(attrs['key'] == "constraint"){
-									constaints += "${attrs['value']}\n"
-								}else if(attrs['key'] == "afterLoad"){
-									afterLoad += "${attrs['value']}\n"
-								}else if(attrs['key'] == "toString"){
-									toString = "String toString(){${attrs['value']}}"
-								}
-							}
-						}else {
+	
+	protected void setParent(Object parent, Object child) {  
+		if(parent)  
+			parent[parent['name']] = child  
+	}
+	
+	
+	
+	protected Object createNode(Object name) {  
+		createNode name, null, null  
+	}  
+	
+	protected Object createNode(Object name, Object value) {  
+		createNode name, null, value  
+	}  
+	
+	protected Object createNode(Object name, Map attrs) {  
+		createNode name, attrs, null  
+	}  
+	
+	protected Object createNode(Object name, Map attrs, Object value) {
+		switch(state){
+			case CLASS:
+			if(attrs && attrs['packageName']) out.println "package ${attrs['packageName']}"
+			out.print "class ${name} "
+			
+			if(attrs && attrs['parent']) out.print "extends ${attrs['parent']}"
+			out.println "{"
+			out.incrementIndent()
+			state = ATTRS
+			break
+			case ATTRS:
+			if(name == "annotation"){
+				if(attrs['annotation']){
+					switch(attrs['annotation'].source){
+						case "http://opennoark.machina.no/searchable":
 						
-							if(attrs['multiplicity'] =~ /.*-M/){
-								hasMany[name] = attrs['type']
-							} else {
-								out.println "${attrs['type']} ${name}"
+							def searchable = "static searchable ="
+							attrs['annotation'].details.keySet().each{ skey ->
+								if(skey == "except")
+									searchable += " [except: ['${attrs['annotation'].details.get(skey).split(',').join('\',\'')}']]"
 							}
-							switch(attrs['multiplicity']){
-								case "1":
-									constaints += "${name}(nullable: false)\n"
-									break
-								case "1-M":
-									constaints += "${name}(minSize: 1)\n"
-									break
-								case "0-1":
-								case "0-M":
-									constaints += "${name}(nullable: true)\n"
-									break
-							}
-							if(attrs['unique'] != null){
-								constaints += "${name}(unique: ${attrs['unique']})\n"
-							}
-						}
+							if(searchable == "static searchable =") searchable += " true"
+							extras = "${extras}${searchable}\n"
+							break
+					}
+				} else {
+					if(attrs['key'] == "documentation"){
+						writer.buffer.insert(0,"/**\n${attrs['value']}\n*/\n")
+					}else if(attrs['key'] == "mapping"){
+						
+						mappings = "${mappings}${attrs['value']}\n"
+					}else if(attrs['key'] == "attribute"){
+						extras = "${extras}${attrs['value']}\n"
+					}else if(attrs['key'] == "transient"){
+						extras = "${extras}${attrs['value']}\n"
+						transients << "\"${attrs['value'].tokenize()[1]}\""
+					}else if(attrs['key'] == "constraint"){
+						constaints += "${attrs['value']}\n"
+					}else if(attrs['key'] == "afterLoad"){
+						afterLoad += "${attrs['value']}\n"
+					}else if(attrs['key'] == "toString"){
+						toString = "String toString(){${attrs['value']}}"
+					}
+				}
+			}else {
+				
+				if(attrs['multiplicity'] =~ /.*-M/){
+					hasMany[name] = attrs['type']
+				} else {
+					out.println "${attrs['type']} ${name}"
+				}
+				switch(attrs['multiplicity']){
+					case "1":
+						constaints += "${name}(nullable: false)\n"
+						break
+					case "1-M":
+						constaints += "${name}(minSize: 1)\n"
+						break
+					case "0-1":
+					case "0-M":
+						constaints += "${name}(nullable: true)\n"
 						break
 				}
-//				println "name: ${name}"
-//				println "value ${value}"
-        return attrs;
-   }
-
+				if(attrs['unique'] != null){
+					constaints += "${name}(unique: ${attrs['unique']})\n"
+				}
+			}
+			break
+		}
+		return attrs;
+	}
+	
 	protected void nodeCompleted(Object parent, Object node) {
 		if(!node || (!node['multiplicity']  && !node['key'] && !node['annotation'] )) {
-			out.println "static constraints = {" //\n${constaints}}"
+			out.println "static constraints = {"
 			out.incrementIndent()
 			constaints.eachLine{
 				out.println it
@@ -137,51 +132,35 @@ class GormBuilder extends BuilderSupport {
 			out.println "}"
 			
 			out.println "static hasMany = ${hasMany.toString()}"
-
+			
 			out.println "static mapping = {"
 			out.incrementIndent()
-			println("MAPPINGS: ${mappings}")
+			
 			mappings.eachLine{
 				out.println it
 			}
 			out.decrementIndent()
 			out.println "}"
-			println("EXTRAS: ${extras}")
+			
 			extras.eachLine { 
 				out.println it
 			}
-
+			
 			if(transients.size() > 0)
 				out.println "static transients = ${transients}"
-
+			
 			if(afterLoad.length() > 0){
 				out.println "def afterLoad = {"
 				out.incrementIndent()
-					afterLoad.eachLine { out.println it }
+				afterLoad.eachLine { out.println it }
 				out.decrementIndent()
 				out.println "}"
 			}
-
+			
 			if(toString.length() > 0) out.println toString
 			
-		    out.decrementIndent()
+			out.decrementIndent()
 			out.println "}"
 		}
 	}
-}  
-
-/*
-
-StringWriter sw = new StringWriter()
-builder = new GormBuilder(sw);
-
-builder.Arkiv {
-	dill(type: "int", multiplicity: "1")
-	dall(type: "String", multiplicity: "0-1")
-	
-	bla(type: "Custom", multiplicity: "0-M")
-	snoft(type: "Custom", multiplicity: "1-M")
 }
-
-
-println sw;*/
