2c8c27d3ba0b9e9eb2729b34178735c38270ded7
Kai Tverra <kai@tverra.net>
2010-06-04 15:11:03 +0200

    Added beginnings of REST to webservice

--- friark/grails-app/conf/UrlMappings.groovy
+++ friark/grails-app/conf/UrlMappings.groovy
@@ -1,11 +1,20 @@
 class UrlMappings {
     static mappings = {
+        
       "/$controller/$action?/$id?"{
-	      constraints {
-			 // apply constraints here
-		 }
-	 }
+            constraints {
+                // apply constraints here
+            }
+        }        
+     
+      "/ws/$controller/$id?"{
+            controller=$controller
+            action=[POST:"save",GET:"show",PUT:"update",DELETE:"delete"]
+            parseRequest=true
+        }
+     
+
       "/"(view:"/index")
 	  "500"(view:'/error')
-	}
+    }
 }
